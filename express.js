fc01ef755d4621c3fb856608501432caa12e152b
khalidmohamedmohsen <khalid.mohamed.mohsen@gmail.com>
2023-09-02 08:20:13 +0300

    with errors

--- express.js
+++ express.js
@@ -1,8 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+app.listen(3000);
+
 app.set('view engine', 'pug');
-app.set('views','./views');
 
-app.get('/', function(req, res){
-    res.render('index');
- });
+app.use(express.static('public'));  
 
- 
+app.get('/', (req, res) => {
+    res.render('HomePage', { title: 'HomePage'});
+});
+
+app.get('/OurServices', (req, res) => {
+    res.render('OurServices', { title: 'OurServices' });
+});
+
+app.get('/ContactUs', (req, res) => {
+    res.render('ContactUs', { title: 'ContactUS' });
+});
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404' });
+});
